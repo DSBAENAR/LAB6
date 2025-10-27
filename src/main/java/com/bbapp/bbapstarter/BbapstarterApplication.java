5b614b7610aed28a4627c3e7c15f8d6554d8efd6
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-10-27 15:35:23 -0500

    Initialize Spring Boot project

---
+++ src/main/java/com/bbapp/bbapstarter/BbapstarterApplication.java
@@ -0,0 +1,13 @@
+package com.bbapp.bbapstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbapstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbapstarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbapp/bbapstarter/BbapstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbapp.bbapstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbapstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
